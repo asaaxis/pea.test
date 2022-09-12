a318ff214eb314487054a2053e3ce29742d837da
akiasa oshima <asaolr4@gmail.com>
2022-09-12 17:32:58 +0900

    コードの追加、修正しました。

--- peapro.js
+++ peapro.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  function get_achievement() {
+  function score_indicate() {
     let subject_points = [Number($('#national_language').val()),
     Number($('#english').val()),
     Number($('#mathematics').val()),
@@ -7,26 +7,37 @@ $(document).ready(function () {
     Number($('#society').val()),
     ];
 
-    let sum = sum + subject_points[0];
+    let sum = subject_points[0];
     sum = sum + subject_points[1];
     sum = sum + subject_points[2];
     sum = sum + subject_points[3];
     sum = sum + subject_points[4]; 
 
     $("#sum_indicate").text(sum);
-  }
+    let average = sum / subject_points.length
+    $('#average_indicate').text(average);
+  };
 
   function get_achievement() {
-    let avarageIndicate = $("#average_indicate").text();
-    console.log(avarageIndicate)
-    if(avarageIndicate >= 80) {
+    let averageIndicate = $("#average_indicate").text();
+    console.log(averageIndicate)
+    if(averageIndicate >= 80) {
       return "A"
-    } else if(avarageIndicate >= 60) {
+    } else if(averageIndicate >= 60) {
         return "B"
-      }  else if(avarageIndicate >= 40) {
+      }  else if(averageIndicate >= 40) {
            return "C"   
          } else {
              return "D"
            }  
   };
+  $('#national_language, #english, #mathematics, #science, #society').change(function () {
+    score_indicate();
+  });
+  
+  $('#btn-evaluation').click(function () {
+    $("#evaluation").text(get_achievement());
+  });
 });
+
+
